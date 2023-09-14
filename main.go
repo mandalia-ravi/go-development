3a7898b0e08338b407d7813dbbea580861bbd4e7
Ravi Mandalia <ravimandalia10@gmail.com>
2023-09-14 10:00:11 +0530

    get handler to get employee by id

---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+type Employee struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+func main() {
+	// connect to database
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// create the table if it doesn't exist
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS employees (id SERIAL PRIMARY KEY, name TEXT, email TEXT)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// create router
+	router := mux.NewRouter()
+	router.HandleFunc("/employees", getEmployees(db)).Methods("GET")
+	router.HandleFunc("/employees/{id}", getEmployee(db)).Methods("GET")
+	router.HandleFunc("/employees", createEmployee(db)).Methods("POST")
+	router.HandleFunc("/employees/{id}", updateEmployee(db)).Methods("PUT")
+	router.HandleFunc("/employees/{id}", deleteEmployee(db)).Methods("DELETE")
+
+}
+
+// get all employees
+func getEmployees(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+// get employee by id
+func getEmployee(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id := vars["id"]
+
+		var e Employee
+		err := db.QueryRow("SELECT * FROM employees WHERE id = $1", id).Scan(&e.ID, &e.Name, &e.Email)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		json.NewEncoder(w).Encode(e)
+	}
+}
+
+// create employee
+func createEmployee(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var e Employee
+		json.NewDecoder(r.Body).Decode(&e)
+
+		err := db.QueryRow("INSERT INTO employees (name, email) VALUES ($1, $2) RETURNING id", e.Name, e.Email).Scan(&e.ID)
+		if err != nil {
+			log.Fatal(err)
+		}
+		json.NewEncoder(w).Encode(e)
+	}
+}
+
+// delete an employee
+func updateEmployee(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var e Employee
+		json.NewDecoder(r.Body).Decode(&e)
+
+		vars := mux.Vars(r)
+		id := vars["id"]
+
+		_, err := db.Exec("UPDATE employees SET name = $1, email = $2 WHERE id = $3", e.Name, e.Email, id)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		json.NewEncoder(w).Encode(e)
+	}
+}
+
+func deleteEmployee(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
